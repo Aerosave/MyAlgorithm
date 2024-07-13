6590fbb86dd0fd0a4d9276afa277439aacc0fa5b
Nikita Anisimov <nikitanisimov98@gmail.com>
2024-07-13 22:09:57 +1000

    Добавьте файлы проекта.

---
+++ MyAlgorithm/BD/DataBase.cs
@@ -0,0 +1,32 @@
+﻿using MyAlgorithm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAlgorithm.BD
+{
+    internal class DataBase
+    {
+        private List<User> users = new List<User>
+    {
+        new User { Login = "qwerty228", Name = "Василий Петрович", IsPremium = true },
+        new User { Login = "PRO_1337", Name = "Аркадий", IsPremium =  false},
+        new User { Login = "SVEN_KILLER", Name = "Катя", IsPremium = true }
+    };
+
+        public User GetUserByLogin(string login)
+        {
+            foreach (var user in users)
+            {
+                if (user.Login == login)
+                {
+                    return user;
+                }
+            }
+            return null;
+        }
+    }
+
+}
---
+++ MyAlgorithm/Program.cs
@@ -0,0 +1,90 @@
+﻿using MyAlgorithm.BD;
+using System.Runtime.ConstrainedExecution;
+
+namespace MyAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataBase users = new DataBase();
+
+            while (true)
+            {
+                // Ввод логина и проверка премиум-статуса
+                Console.WriteLine("Введите 'Login' от своей учетной записи. Пароль не нужен, мы вам и так верим:");
+                string login = Console.ReadLine();
+                User user = users.GetUserByLogin(login);
+
+                if (user == null)
+                {
+                    ShowAds();
+                    Console.WriteLine("Пользователь не найден. Попробуйте снова.");
+                    continue; // Возвращаемся в начало цикла
+                }
+
+                switch (user)
+                {
+                    case User u when u.IsPremium:
+                        Console.WriteLine($"Добрый день, {user.Name}");
+                        break;
+                    default:
+                        ShowAds();
+                        Console.WriteLine($"Добрый день, {user.Name}");
+                        Thread.Sleep(1000);
+                        Console.WriteLine("Хотите купить премиум? (да/нет)");
+                        string response = Console.ReadLine();
+                        if (response.ToLower() == "да")
+                        {
+                            BuyPremium(user);
+                            Console.WriteLine("Премиум-статус успешно активирован!");
+                        }
+                        break;
+                }
+                while (true)
+                {
+                    Console.WriteLine("Хотите попробовать снова? (да/нет)");
+                    string responsecontin = Console.ReadLine();
+                    if (responsecontin.ToLower() == "да")
+                    {
+                        break; // Возвращаемся в начало цикла
+                    }
+                    else if (responsecontin.ToLower() == "нет")
+                    {
+                        Console.WriteLine("Прощайте!!!");
+                        Thread.Sleep(3000);
+                        return; // Выход из программы
+                    }
+                    else
+                    {
+                        Console.WriteLine("Пожалуйста, введите 'да' или 'нет'.");
+                    }
+                }
+            }
+
+        }
+        static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
+        }
+
+        static void BuyPremium(User user) 
+        {
+
+            user.IsPremium = true;
+
+        }
+
+        
+    }
+}
